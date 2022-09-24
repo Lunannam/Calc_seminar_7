996592fefa04567b9255890e12a38d838fb767fd
Denis <9581886@gmail.com>
2022-09-24 19:56:15 +0300

    Изменения в controller.py

--- logs.py
+++ logs.py
@@ -5,5 +5,5 @@ def logger(data, result):
     Записывает время операции, саму операцию и её результат
     """
     time = dt.now().strftime('%m.%d.%Y - %H:%M')
-    with open('log.csv', 'a') as file:
+    with open('log.csv', 'a', encoding = 'UTF-8') as file:
         file.write(f'{time}: {data} = {result}\n')
