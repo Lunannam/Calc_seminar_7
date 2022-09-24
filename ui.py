e0ff23c79ab3b27270ccf64bb131aac8abbdecba
EgorEvtushenko <ljvbyj0990@gmail.com>
2022-09-24 19:15:56 +0500

    Сделал модули пользовательского интерфейса и проверок

--- ui.py
+++ ui.py
@@ -1,1 +1,32 @@
-# 
+import check_number as f
+
+def rational_number(number:str) -> float:
+    '''
+    Ввод рационального числа
+    '''
+
+    num = f.check_float_number(number)
+    return num
+
+def complex_number(number:str) -> complex:
+    '''
+    Ввод комплексного числа
+    '''
+
+    num = f.check_int_number(number)
+    return num
+
+def operation(oper:str) -> str:
+    '''
+    Ввод операции
+    '''
+    symb = f.check_symbol(oper)
+    return symb
+
+def choice_calc(number:str) -> int:
+    '''
+    Выбор калькулятора
+    '''
+
+    calc = f.check_symbol(number)
+    return calc
