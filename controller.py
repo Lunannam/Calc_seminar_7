48703e192d7c601f8d0fd1f7f828c28c6712a67a
EgorEvtushenko <ljvbyj0990@gmail.com>
2022-09-26 20:02:15 +0500

    Убрали функцию msg_compl

--- controller.py
+++ controller.py
@@ -3,21 +3,6 @@ import ui
 import logs
 
 
-def msg_compl(first_r, first_mn, second_r, second_mn, sign):
-    '''
-    Функция получает данные и формирует строку из них
-    '''
-    if first_mn >= 0:
-        data_log = '(' + str(first_r) + ' + ' + str(first_mn) + 'j' + ')' + ' ' + sign + ' ' + '(' + str(second_r)
-    elif first_mn < 0:
-        data_log = '(' + str(first_r) + ' - ' + str(first_mn*(-1)) + 'j' + ')' + ' ' + sign + ' ' + '(' + str(second_r)
-    if second_mn >= 0:
-        data_log += ' + ' + str(second_mn) + 'j' + ')'
-    if second_mn < 0:
-        data_log += ' - ' + str(second_mn * (-1)) + 'j' + ')'
-    return data_log
-
-
 def button_click():
     '''
     Функция запрашивает данные, решает и выводит
@@ -29,13 +14,11 @@ def button_click():
         second_r = ui.rational_number(2)
         second_mn = 0
         calcul.init_ratio(first_r, second_r)
-        data_log = '' + str(first_r) + ' ' + sign + ' ' + str(second_r)
     elif name == 2:
         first_r, first_mn = ui.complex_number(1)
         sign = ui.operation('Введите знак операции: (+, -, *, /): ')
         second_r, second_mn = ui.complex_number(2)
         calcul.init_compl(first_r, first_mn, second_r, second_mn)
-        data_log = msg_compl(first_r, first_mn, second_r, second_mn, sign)
     
     if sign == '+':
         result = calcul.sum()
@@ -49,5 +32,6 @@ def button_click():
             result = 'Деление на 0 невозможно!'
             print(result)
 
+    data_log = str(calcul.x) + ' ' + sign + ' ' + str(calcul.y)
     ui.output_result(data_log, result)
     logs.logger(data_log, result)
